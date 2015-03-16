3c7ae55c61888a241c96fe47470d6065e3b93f91
Billy Cravens <bdcravens@gmail.com>
2015-03-16 03:01:08 -0500

    http://emberjs.com/guides/getting-started/adding-child-routes/

--- js/router.js
+++ js/router.js
@@ -1,10 +1,17 @@
 Todos.Router.map(function() {
-    this.resource('todos', { path: '/' }),
-    this.route('foo')
+    this.resource('todos', { path: '/' }, function() {
+        // additional child routes will go here later
+    });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('todo');
+    }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+    model: function() {
+        return this.modelFor('todos');
     }
 });
